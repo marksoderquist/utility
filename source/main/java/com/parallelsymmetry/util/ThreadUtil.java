3f8fb51b44e1977b1fb48b509a42ba4b004dc684
Mark Soderquist <mark@soderquist.net>
2008-02-13 15:51:05 +0000

    Add a method to append stack trace information to an existing throwable.

--- source/main/java/com/parallelsymmetry/util/ThreadUtil.java
+++ source/main/java/com/parallelsymmetry/util/ThreadUtil.java
@@ -11,4 +11,17 @@ public class ThreadUtil {
 		Thread.yield();
 	}
 
+	public static final Throwable appendStackTrace( Throwable original, Throwable parent ) {
+		if( original == null ) return parent;
+		if( parent != null ) {
+			StackTraceElement[] originalStack = original.getStackTrace();
+			StackTraceElement[] parentStack = parent.getStackTrace();
+			StackTraceElement[] elements = new StackTraceElement[originalStack.length + parentStack.length];
+			System.arraycopy( originalStack, 0, elements, 0, originalStack.length );
+			System.arraycopy( parentStack, 0, elements, originalStack.length, parentStack.length );
+			original.setStackTrace( elements );
+		}
+		return original;
+	}
+
 }
--- source/test/java/com/parallelsymmetry/util/ThreadUtilTest.java
+++ source/test/java/com/parallelsymmetry/util/ThreadUtilTest.java
@@ -1,7 +1,5 @@
 package com.parallelsymmetry.util;
 
-import com.parallelsymmetry.util.ThreadUtil;
-
 import junit.framework.TestCase;
 
 public class ThreadUtilTest extends TestCase {
@@ -12,6 +10,38 @@ public class ThreadUtilTest extends TestCase {
 		ThreadUtil.pause( length );
 		long stop = System.currentTimeMillis();
 		assertTrue( stop - start >= length );
+	}
+
+	public void testAppendStackTraceWithNullOriginal() throws Exception {
+		Throwable throwable2 = new Throwable();
+		StackTraceElement[] stack2 = throwable2.getStackTrace();
+		assertEquals( stack2, ThreadUtil.appendStackTrace( null, throwable2 ).getStackTrace() );
+	}
+
+	public void testAppendStackTraceWithNullParent() throws Exception {
+		Throwable throwable1 = new Throwable();
+		StackTraceElement[] stack1 = throwable1.getStackTrace();
+		assertEquals( stack1, ThreadUtil.appendStackTrace( throwable1, null ).getStackTrace() );
+	}
+
+	public void testAppendStackTrace() throws Exception {
+		Throwable throwable1 = new Throwable();
+		Throwable throwable2 = new Throwable();
+
+		StackTraceElement[] stack1 = throwable1.getStackTrace();
+		StackTraceElement[] stack2 = throwable2.getStackTrace();
+
+		StackTraceElement[] elements = new StackTraceElement[stack1.length + stack2.length];
+		System.arraycopy( stack1, 0, elements, 0, stack1.length );
+		System.arraycopy( stack2, 0, elements, stack1.length, stack2.length );
+
+		assertEquals( elements, ThreadUtil.appendStackTrace( throwable1, throwable2 ).getStackTrace() );
+	}
+
+	private void assertEquals( Object[] array1, Object[] array2 ) {
+		for( int index = 0; index < array1.length; index++ ) {
+			assertEquals( array1[index], array2[index] );
+		}
 	}
 
 }
