f93843d34b0ba3f05a5ab06616da9110f3e70b21
Mark Soderquist <mark@soderquist.net>
2008-09-23 22:00:31 +0000

    Add event handling to the counting stream and reader.

--- source/main/java/com/parallelsymmetry/util/CountingInputStream.java
+++ source/main/java/com/parallelsymmetry/util/CountingInputStream.java
@@ -2,6 +2,7 @@ package com.parallelsymmetry.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class CountingInputStream extends InputStream {
@@ -16,6 +17,16 @@ public class CountingInputStream extends InputStream {
 
 	public long getCount() {
 		return count.get();
+	}
+
+	private Set<CountingListener> listeners;
+
+	public void addCountingListener( CountingListener listener ) {
+		listeners.add( listener );
+	}
+
+	public void removeCountingListener( CountingListener listener ) {
+		listeners.remove( listener );
 	}
 
 	public int available() throws IOException {
@@ -43,23 +54,20 @@ public class CountingInputStream extends InputStream {
 	}
 
 	public int read() throws IOException {
-		// FIXME Add event handling.
 		int read = input.read();
-		if( read > 0 ) count.incrementAndGet();
+		if( read > 0 ) triggerCountingEvent( count.incrementAndGet() );
 		return read;
 	}
 
 	public int read( byte[] b, int off, int len ) throws IOException {
-		// FIXME Add event handling.
 		int read = input.read( b, off, len );
-		if( read > 0 ) count.addAndGet( read );
+		if( read > 0 ) triggerCountingEvent( count.addAndGet( read ) );
 		return read;
 	}
 
 	public int read( byte[] b ) throws IOException {
-		// FIXME Add event handling.
 		int read = input.read( b );
-		if( read > 0 ) count.addAndGet( read );
+		if( read > 0 ) triggerCountingEvent( count.addAndGet( read ) );
 		return read;
 	}
 
@@ -73,6 +81,16 @@ public class CountingInputStream extends InputStream {
 
 	public String toString() {
 		return input.toString();
+	}
+
+	protected void triggerCountingEvent( long count ) {
+		fireCountingEvent( new CountingEvent( count ) );
+	}
+
+	protected void fireCountingEvent( CountingEvent event ) {
+		for( CountingListener listener : listeners ) {
+			listener.countUpdated( event );
+		}
 	}
 
 }
--- source/main/java/com/parallelsymmetry/util/CountingReader.java
+++ source/main/java/com/parallelsymmetry/util/CountingReader.java
@@ -3,6 +3,7 @@ package com.parallelsymmetry.util;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.CharBuffer;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class CountingReader extends Reader {
@@ -17,6 +18,16 @@ public class CountingReader extends Reader {
 
 	public long getCount() {
 		return count.get();
+	}
+
+	private Set<CountingListener> listeners;
+
+	public void addCountingListener( CountingListener listener ) {
+		listeners.add( listener );
+	}
+
+	public void removeCountingListener( CountingListener listener ) {
+		listeners.remove( listener );
 	}
 
 	public void close() throws IOException {
@@ -40,30 +51,26 @@ public class CountingReader extends Reader {
 	}
 
 	public int read() throws IOException {
-		// FIXME Add event handling.
 		int read = reader.read();
-		if( read > 0 ) count.incrementAndGet();
+		if( read > 0 ) triggerCountingEvent( count.incrementAndGet() );
 		return read;
 	}
 
 	public int read( char[] cbuf, int off, int len ) throws IOException {
-		// FIXME Add event handling.
 		int read = reader.read( cbuf, off, len );
-		if( read > 0 ) this.count.addAndGet( read );
+		if( read > 0 ) triggerCountingEvent( this.count.addAndGet( read ) );
 		return read;
 	}
 
 	public int read( char[] cbuf ) throws IOException {
-		// FIXME Add event handling.
 		int read = reader.read( cbuf );
-		if( read > 0 ) this.count.addAndGet( read );
+		if( read > 0 ) triggerCountingEvent( this.count.addAndGet( read ) );
 		return read;
 	}
 
 	public int read( CharBuffer target ) throws IOException {
-		// FIXME Add event handling.
 		int read = reader.read( target );
-		if( read > 0 ) this.count.addAndGet( read );
+		if( read > 0 ) triggerCountingEvent( this.count.addAndGet( read ) );
 		return read;
 	}
 
@@ -81,6 +88,16 @@ public class CountingReader extends Reader {
 
 	public String toString() {
 		return reader.toString();
+	}
+
+	protected void triggerCountingEvent( long count ) {
+		fireCountingEvent( new CountingEvent( count ) );
+	}
+
+	protected void fireCountingEvent( CountingEvent event ) {
+		for( CountingListener listener : listeners ) {
+			listener.countUpdated( event );
+		}
 	}
 
 }
