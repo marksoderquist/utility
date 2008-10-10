0213c92b66231db623a61152c1b4f0d9c45ad791
Mark Soderquist <mark@soderquist.net>
2008-10-10 20:27:46 +0000

    Add capability to copy files and folders as well as mark them for deletion on exit.

--- source/main/java/com/parallelsymmetry/util/FileUtil.java
+++ source/main/java/com/parallelsymmetry/util/FileUtil.java
@@ -1,6 +1,9 @@
 package com.parallelsymmetry.util;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class FileUtil {
 
@@ -16,6 +19,84 @@ public class FileUtil {
 		return name.substring( index + 1 );
 	}
 
+	/**
+	 * Create a temporary folder. If there is a problem creating the folder this
+	 * method will return null.
+	 * 
+	 * @param prefix
+	 * @param suffix
+	 * @return
+	 * @throws IOException
+	 */
+	public static final File createTempFolder( String prefix, String suffix ) throws IOException {
+		File file = File.createTempFile( prefix, suffix );
+		if( !file.delete() ) return null;
+		if( !file.mkdir() ) return null;
+		return file;
+	}
+
+	/**
+	 * Create a temporary folder. If there is a problem creating the folder this
+	 * method will return null.
+	 * 
+	 * @param prefix
+	 * @param suffix
+	 * @param parent
+	 * @return
+	 * @throws IOException
+	 */
+	public static final File createTempFolder( String prefix, String suffix, File parent ) throws IOException {
+		File file = File.createTempFile( prefix, suffix, parent );
+		if( !file.delete() ) return null;
+		if( !file.mkdir() ) return null;
+		return file;
+	}
+
+	public static final boolean copy( File source, File target ) throws IOException {
+		return copy( source, target, false );
+	}
+
+	public static final boolean copy( File source, File target, boolean keepSourceFolder ) throws IOException {
+		// Cannot copy sources or targets that don't exist.
+		if( !source.exists() || !target.exists() ) return false;
+
+		// Cannot copy a folder to a file.
+		if( source.isDirectory() && target.isFile() ) return false;
+
+		// Copy file sources to file targets.
+		if( source.isFile() && target.isFile() ) {
+			System.out.println( "copy: " + source + " to " + target );
+			FileInputStream input = new FileInputStream( source );
+			FileOutputStream output = new FileOutputStream( target );
+			IOPump pump = new IOPump( input, output );
+			pump.startAndWait( 0 );
+			return true;
+		}
+
+		// Copy file sources to folder targets.
+		if( source.isFile() && target.isDirectory() ) {
+			File newTarget = new File( target, source.getName() );
+			newTarget.createNewFile();
+			return copy( source, newTarget );
+		}
+
+		// Copy folder sources to folder targets.
+		if( source.isDirectory() && target.isDirectory() ) {
+			File newTarget = target;
+			if( keepSourceFolder ) {
+				newTarget = new File( target, source.getName() );
+				newTarget.mkdir();
+			}
+			boolean result = true;
+			for( File file : source.listFiles() ) {
+				result = result & copy( file, newTarget, true );
+			}
+			return result;
+		}
+
+		return false;
+	}
+
 	public static final boolean deleteTree( File file ) {
 		if( !file.exists() ) return true;
 		if( file.isDirectory() ) {
@@ -24,6 +105,16 @@ public class FileUtil {
 			}
 		}
 		return file.delete();
+	}
+
+	public static final void deleteTreeOnExit( File file ) {
+		if( !file.exists() ) return;
+		file.deleteOnExit();
+		if( file.isDirectory() ) {
+			for( File child : file.listFiles() ) {
+				deleteTreeOnExit( child );
+			}
+		}
 	}
 
 }
--- source/test/java/com/parallelsymmetry/util/FileUtilTest.java
+++ source/test/java/com/parallelsymmetry/util/FileUtilTest.java
@@ -1,10 +1,27 @@
 package com.parallelsymmetry.util;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 
 import junit.framework.TestCase;
 
 public class FileUtilTest extends TestCase {
+
+	private static final String PREFIX = "test";
+
+	private static final FilenameFilter TEST_FILE_FILTER = new TestFilenameFilter();
+
+	@Override
+	public void tearDown() throws Exception {
+		File tmp = new File( System.getProperty( "java.io.tmpdir" ) );
+		for( File file : tmp.listFiles( TEST_FILE_FILTER ) ) {
+			FileUtil.deleteTree( file );
+		}
+	}
 
 	public void testGetExtensionWithFile() throws Exception {
 		assertEquals( "Incorrect extension.", null, FileUtil.getExtension( (File)null ) );
@@ -18,8 +35,112 @@ public class FileUtilTest extends TestCase {
 		assertEquals( "Incorrect extension.", "txt", FileUtil.getExtension( "test.txt" ) );
 	}
 
+	public void testCreateTempFolder() throws Exception {
+		File folder = FileUtil.createTempFolder( PREFIX, "createTempFolder" );
+		assertTrue( folder.exists() );
+		String name = folder.getName();
+		File check = new File( System.getProperty( "java.io.tmpdir" ), name );
+		assertEquals( check, folder );
+		assertTrue( check.exists() );
+		folder.delete();
+	}
+
+	public void testCopyFileToFile() throws Exception {
+		long time = System.currentTimeMillis();
+		File source = File.createTempFile( PREFIX, "copyFileToFileSource" );
+		File target = File.createTempFile( PREFIX, "copyFileToFileTarget" );
+		FileOutputStream fileOutput = new FileOutputStream( source );
+		DataOutputStream output = new DataOutputStream( fileOutput );
+		output.writeLong( time );
+		output.close();
+
+		assertTrue( FileUtil.copy( source, target ) );
+
+		FileInputStream fileInput = new FileInputStream( target );
+		DataInputStream input = new DataInputStream( fileInput );
+		assertEquals( time, input.readLong() );
+		input.close();
+
+		assertTrue( source.delete() );
+		assertTrue( target.delete() );
+	}
+
+	public void testCopyFileToFolder() throws Exception {
+		long time = System.currentTimeMillis();
+		File source = File.createTempFile( PREFIX, "copyFileToFolderSource" );
+		File target = FileUtil.createTempFolder( PREFIX, "copyFileToFolderTarget" );
+		FileOutputStream fileOutput = new FileOutputStream( source );
+		DataOutputStream output = new DataOutputStream( fileOutput );
+		output.writeLong( time );
+		output.close();
+
+		assertTrue( FileUtil.copy( source, target ) );
+
+		File child = new File( target, source.getName() );
+		FileInputStream fileInput = new FileInputStream( child );
+		DataInputStream input = new DataInputStream( fileInput );
+		assertEquals( time, input.readLong() );
+		input.close();
+	}
+
+	public void testCopyFolderToFile() throws Exception {
+		File source = FileUtil.createTempFolder( PREFIX, "copyFolderToFileSource" );
+		File target = File.createTempFile( PREFIX, "copyFolderToFileTarget" );
+		assertFalse( FileUtil.copy( source, target ) );
+		assertTrue( source.delete() );
+		assertTrue( target.delete() );
+	}
+
+	public void testCopyFolderToFolder() throws Exception {
+		File parent0 = FileUtil.createTempFolder( PREFIX, "copyFolderToFolderParent0" );
+		File parent1 = FileUtil.createTempFolder( PREFIX, "copyFolderToFolderParent1", parent0 );
+		File leaf0 = File.createTempFile( PREFIX, "copyFolderToFolderLeaf0", parent0 );
+		File leaf1 = File.createTempFile( PREFIX, "copyFolderToFolderLeaf1", parent0 );
+		File leaf2 = File.createTempFile( PREFIX, "copyFolderToFolderLeaf2", parent1 );
+		File leaf3 = File.createTempFile( PREFIX, "copyFolderToFolderLeaf3", parent1 );
+		assertEquals( 3, parent0.listFiles().length );
+		assertEquals( 2, parent1.listFiles().length );
+
+		File target = FileUtil.createTempFolder( PREFIX, "copyFolderToFolderTarget" );
+
+		assertTrue( FileUtil.copy( parent0, target, false ) );
+
+		File target1 = new File( target, parent1.getName() );
+		assertEquals( 3, target.listFiles().length );
+		assertEquals( 2, target1.listFiles().length );
+		assertTrue( new File( target, leaf0.getName() ).exists() );
+		assertTrue( new File( target, leaf1.getName() ).exists() );
+		assertTrue( new File( target1, leaf2.getName() ).exists() );
+		assertTrue( new File( target1, leaf3.getName() ).exists() );
+	}
+
+	public void testCopyFolderToFolderWithSourceFolder() throws Exception {
+		File parent0 = FileUtil.createTempFolder( PREFIX, "copyFolderToFolderParent0" );
+		File parent1 = FileUtil.createTempFolder( PREFIX, "copyFolderToFolderParent1", parent0 );
+		File leaf0 = File.createTempFile( PREFIX, "copyFolderToFolderLeaf0", parent0 );
+		File leaf1 = File.createTempFile( PREFIX, "copyFolderToFolderLeaf1", parent0 );
+		File leaf2 = File.createTempFile( PREFIX, "copyFolderToFolderLeaf2", parent1 );
+		File leaf3 = File.createTempFile( PREFIX, "copyFolderToFolderLeaf3", parent1 );
+		assertEquals( 3, parent0.listFiles().length );
+		assertEquals( 2, parent1.listFiles().length );
+
+		File target = FileUtil.createTempFolder( PREFIX, "copyFolderToFolderTarget" );
+
+		assertTrue( FileUtil.copy( parent0, target, true ) );
+
+		File target0 = new File( target, parent0.getName() );
+		File target1 = new File( target0, parent1.getName() );
+		assertEquals( 3, target0.listFiles().length );
+		assertEquals( 2, target1.listFiles().length );
+		assertTrue( new File( target0, leaf0.getName() ).exists() );
+		assertTrue( new File( target0, leaf1.getName() ).exists() );
+		assertTrue( new File( target1, leaf2.getName() ).exists() );
+		assertTrue( new File( target1, leaf3.getName() ).exists() );
+	}
+
 	public void testDeleteTree() throws Exception {
-		File temp = File.createTempFile( "test", "" ).getParentFile();
+		File file = File.createTempFile( PREFIX, "deleteTree" );
+		File temp = file.getParentFile();
 		File parent = new File( temp, "parent" );
 		parent.mkdirs();
 		File child = new File( parent, "child" );
@@ -31,6 +152,17 @@ public class FileUtilTest extends TestCase {
 		FileUtil.deleteTree( parent );
 		assertFalse( parent.exists() );
 		assertFalse( child.exists() );
+
+		assertTrue( file.delete() );
+	}
+
+	private static final class TestFilenameFilter implements FilenameFilter {
+
+		@Override
+		public boolean accept( File dir, String name ) {
+			return name.startsWith( PREFIX );
+		}
+
 	}
 
 }
