e76911e64087ad2b00113cb655cb377e658aa883
Mark Soderquist <mark@soderquist.net>
2008-11-21 01:57:42 +0000

    Add XML formatting method.

--- source/main/java/com/parallelsymmetry/util/XmlUtil.java
+++ source/main/java/com/parallelsymmetry/util/XmlUtil.java
@@ -2,9 +2,16 @@ package com.parallelsymmetry.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
@@ -15,6 +22,23 @@ public class XmlUtil {
 		if( stream == null ) return null;
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		return factory.newDocumentBuilder().parse( stream );
+	}
+
+	public static final void format( InputStream input, OutputStream output ) throws IOException {
+		format( input, output, 2 );
+	}
+
+	public static final void format( InputStream input, OutputStream output, int indentAmount ) throws IOException {
+		Transformer transformer;
+		TransformerFactory factory = TransformerFactory.newInstance();
+		try {
+			transformer = factory.newTransformer();
+			transformer.setOutputProperty( OutputKeys.INDENT, "yes" );
+			transformer.setOutputProperty( "{http://xml.apache.org/xslt}indent-amount", String.valueOf( indentAmount ) );
+			transformer.transform( new StreamSource( input ), new StreamResult( output ) );
+		} catch( TransformerException exception ) {
+			throw new IOException( exception );
+		}
 	}
 
 }
--- source/test/java/com/parallelsymmetry/util/XmlUtilTest.java
+++ source/test/java/com/parallelsymmetry/util/XmlUtilTest.java
@@ -1,5 +1,7 @@
 package com.parallelsymmetry.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
 import junit.framework.TestCase;
@@ -20,6 +22,28 @@ public class XmlUtilTest extends TestCase {
 	public void testLoadXmlDocumentWithNullStream() throws Exception {
 		Document document = XmlUtil.loadXmlDocument( null );
 		assertNull( document );
+	}
+
+	@Test
+	public void testFormat() throws Exception {
+		String data = "<tag><indent/></tag>";
+		String test = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<tag>\n  <indent/>\n</tag>";
+		ByteArrayInputStream input = new ByteArrayInputStream( data.getBytes() );
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+		XmlUtil.format( input, output );
+		assertEquals( test, output.toString().trim() );
+	}
+
+	@Test
+	public void testFormatWithIndentSize() throws Exception {
+		String data = "<tag><indent/></tag>";
+		String test = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<tag>\n   <indent/>\n</tag>";
+		ByteArrayInputStream input = new ByteArrayInputStream( data.getBytes() );
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+		XmlUtil.format( input, output, 3 );
+		assertEquals( test, output.toString().trim() );
 	}
 
 }
