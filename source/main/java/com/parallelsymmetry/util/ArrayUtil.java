4260590a1fa3aebe5732d0edc652873e9c4e8181
Mark Soderquist <mark@soderquist.net>
2009-04-08 03:16:02 +0000

    Add contains using equals method.

--- source/main/java/com/parallelsymmetry/util/ArrayUtil.java
+++ source/main/java/com/parallelsymmetry/util/ArrayUtil.java
@@ -9,4 +9,12 @@ public class ArrayUtil {
 		return false;
 	}
 
+	public static final <T> boolean containsEquivalent( T[] array, T element ) {
+		for( T test : array ) {
+			if( element == null && test == null ) return true;
+			if( test != null && test.equals( element ) ) return true;
+		}
+		return false;
+	}
+
 }
--- source/test/java/com/parallelsymmetry/util/ArrayUtilTest.java
+++ source/test/java/com/parallelsymmetry/util/ArrayUtilTest.java
@@ -4,8 +4,6 @@ import junit.framework.TestCase;
 
 import org.junit.Test;
 
-import com.parallelsymmetry.util.ArrayUtil;
-
 public class ArrayUtilTest extends TestCase {
 
 	@Test
@@ -23,4 +21,22 @@ public class ArrayUtilTest extends TestCase {
 		assertTrue( ArrayUtil.contains( array, object3 ) );
 	}
 
+	@Test
+	public void testContainsEquivalent() {
+		Object object1 = new Integer( 1 );
+		Object object2 = new Integer( 2 );
+		Object object3 = new Integer( 3 );
+
+		Object[] array = new Object[3];
+		array[0] = object1;
+		array[2] = object3;
+
+		assertTrue( ArrayUtil.containsEquivalent( array, object1 ) );
+		assertFalse( ArrayUtil.containsEquivalent( array, object2 ) );
+		assertTrue( ArrayUtil.containsEquivalent( array, object3 ) );
+
+		assertTrue( ArrayUtil.containsEquivalent( array, new Integer( 1 ) ) );
+		assertFalse( ArrayUtil.containsEquivalent( array, new Integer( 2 ) ) );
+		assertTrue( ArrayUtil.containsEquivalent( array, new Integer( 3 ) ) );
+	}
 }
