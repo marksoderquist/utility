eb65bf71941732ae1fba5bc4c3d6ee4fb3383fdb
Mark Soderquist <mark@soderquist.net>
2009-06-25 13:49:27 +0000

    Work to store guide state.

--- source/main/java/com/parallelsymmetry/util/ArrayUtil.java
+++ source/main/java/com/parallelsymmetry/util/ArrayUtil.java
@@ -17,4 +17,31 @@ public class ArrayUtil {
 		return false;
 	}
 
+	public static final String encodeIntArray( int[] array ) {
+		if( array == null ) return null;
+
+		StringBuilder builder = new StringBuilder();
+
+		for( int index = 0; index < array.length; index++ ) {
+			if( index > 0 ) builder.append( "," );
+			builder.append( Integer.toString( index ) );
+		}
+
+		return builder.toString();
+	}
+
+	public static final int[] decodeIntArray( String string ) {
+		if( string == null ) return null;
+		if( TextUtil.isEmpty( string ) ) return new int[0];
+
+		String[] tokens = string.split( "," );
+
+		int[] array = new int[tokens.length];
+		for( int index = 0; index < array.length; index++ ) {
+			array[index] = Integer.parseInt( tokens[index] );
+		}
+
+		return array;
+	}
+
 }
