ca1d13ad0041605bfbe62f8b7efe5dc864b4a466
Mark Soderquist <mark@soderquist.net>
2010-03-31 01:18:34 +0000

    Add XmlFormatter class.

---
+++ source/main/java/com/parallelsymmetry/util/XmlFormatter.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package com.parallelsymmetry.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class XmlFormatter extends Thread {
+
+	private boolean finished;
+
+	private InputStream input;
+
+	private OutputStream output;
+
+	private int indent;
+
+	private IOException exception;
+
+	public XmlFormatter( InputStream input, OutputStream output ) {
+		this( input, output, 2 );
+	}
+
+	public XmlFormatter( InputStream input, OutputStream output, int indent ) {
+		super( "XML Formatter" );
+
+		this.input = input;
+		this.output = output;
+		this.indent = indent;
+
+		setDaemon( true );
+	}
+
+	public void run() {
+		try {
+			XmlUtil.format( input, output, indent );
+		} catch( IOException exception ) {
+			this.exception = exception;
+		} finally {
+			synchronized( this ) {
+				finished = true;
+				notifyAll();
+			}
+		}
+	}
+
+	public synchronized void waitFor() throws IOException, InterruptedException {
+		long timeout = 100;
+		long delay = timeout;
+		long timestamp = System.currentTimeMillis();
+		while( !finished && ( System.currentTimeMillis() - timestamp < timeout ) ) {
+			wait( delay );
+			delay = 1;
+		}
+		if( exception != null ) throw exception;
+	}
+
+}
