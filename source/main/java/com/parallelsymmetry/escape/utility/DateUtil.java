14350987df5db7275c1dd096585669eff1561419
Mark Soderquist <mark@soderquist.net>
2011-01-20 16:19:45 +0000

    Add currentYear() method to DateUtil.

--- source/main/java/com/parallelsymmetry/escape/utility/DateUtil.java
+++ source/main/java/com/parallelsymmetry/escape/utility/DateUtil.java
@@ -2,10 +2,29 @@ package com.parallelsymmetry.escape.utility;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
 public class DateUtil {
+
+	/**
+	 * Convenience method to get the current year for the UTC time zone.
+	 * 
+	 * @return
+	 */
+	public static final int getCurrentYear() {
+		return getCurrentYear( "UTC" );
+	}
+
+	/**
+	 * Convenience method to get the current year based on time zone.
+	 * @param timezone
+	 * @return
+	 */
+	public static final int getCurrentYear( String timezone ) {
+		return Calendar.getInstance( TimeZone.getTimeZone( timezone ) ).get( Calendar.YEAR );
+	}
 
 	/**
 	 * Parse a date string with the given format using the UTC time zone.
