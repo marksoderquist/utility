63bc806a21dd60efbdd14690debf5dc212ff88f4
Mark Soderquist <mark@soderquist.net>
2011-03-24 17:36:38 +0000

    Move action classes to data node class.

--- source/main/java/com/parallelsymmetry/escape/utility/data/DataNode.java
+++ source/main/java/com/parallelsymmetry/escape/utility/data/DataNode.java
@@ -202,4 +202,52 @@ public abstract class DataNode {
 		if( resources.size() == 0 ) resources = null;
 	}
 
+	private static class SetAttributeAction extends Action {
+
+		private String name;
+
+		private Object oldValue;
+
+		private Object newValue;
+
+		public SetAttributeAction( DataNode data, String name, Object oldValue, Object newValue ) {
+			super( data );
+			this.name = name;
+			this.oldValue = oldValue;
+			this.newValue = newValue;
+		}
+
+		@Override
+		protected ActionResult process() {
+			ActionResult result = new ActionResult( this );
+
+			getData().doSetAttribute( name, oldValue, newValue );
+
+			DataEvent.Type type = DataEvent.Type.MODIFY;
+			type = oldValue == null ? DataEvent.Type.INSERT : type;
+			type = newValue == null ? DataEvent.Type.REMOVE : type;
+			result.addEvent( new DataAttributeEvent( type, getData(), name, oldValue, newValue ) );
+
+			return result;
+		}
+
+	}
+	
+	private static class ClearModifiedAction extends Action {
+
+		public ClearModifiedAction( DataNode data ) {
+			super( data );
+		}
+
+		@Override
+		protected ActionResult process() {
+			ActionResult result = new ActionResult( this );
+
+			getData().doSetModified( false );
+
+			return result;
+		}
+	}
+	
+	
 }
