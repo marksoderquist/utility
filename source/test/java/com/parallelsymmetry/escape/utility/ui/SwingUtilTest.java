785bdff47516449167cc4a20591e4bde7b405e47
Mark Soderquist <mark@soderquist.net>
2011-04-11 17:24:19 +0000

    Add SwingUtil class.

---
+++ source/test/java/com/parallelsymmetry/escape/utility/ui/SwingUtilTest.java
@@ -0,0 +1,66 @@
+package com.parallelsymmetry.escape.utility.ui;
+
+import java.awt.EventQueue;
+
+import junit.framework.TestCase;
+
+public class SwingUtilTest extends TestCase {
+
+	public void testSwingWait() {
+		ExecuteTester tester = new ExecuteTester();
+
+		EventQueue.invokeLater( tester );
+		SwingUtil.swingWait();
+		assertTrue( tester.isDone() );
+	}
+
+	public void testExecuteSafely() {
+		ExecuteTester tester = new ExecuteTester();
+
+		tester.reset();
+		tester.run();
+		assertFalse( tester.isSafe() );
+
+		tester.reset();
+		SwingUtil.executeSafely( tester );
+		tester.waitFor();
+		assertTrue( tester.isSafe() );
+	}
+
+	private static final class ExecuteTester implements Runnable {
+
+		private boolean done;
+
+		private boolean safe;
+
+		public synchronized void run() {
+			safe = EventQueue.isDispatchThread();
+			done = true;
+			notifyAll();
+		}
+
+		public boolean isDone() {
+			return done;
+		}
+
+		public boolean isSafe() {
+			return safe;
+		}
+
+		public void reset() {
+			done = false;
+		}
+
+		public synchronized void waitFor() {
+			while( !done ) {
+				try {
+					wait();
+				} catch( InterruptedException exception ) {
+					return;
+				}
+			}
+		}
+
+	}
+
+}
