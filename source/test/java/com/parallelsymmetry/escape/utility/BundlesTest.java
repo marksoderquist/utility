dbee54363557cf6341909a05ba74aca720eb158b
Mark Soderquist <mark@soderquist.net>
2011-07-28 18:53:32 +0000

    Work on resource bundle resource resolution.

--- source/test/java/com/parallelsymmetry/escape/utility/BundlesTest.java
+++ source/test/java/com/parallelsymmetry/escape/utility/BundlesTest.java
@@ -1,5 +1,8 @@
 package com.parallelsymmetry.escape.utility;
 
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Locale;
 
 import junit.framework.TestCase;
@@ -7,6 +10,10 @@ import junit.framework.TestCase;
 public class BundlesTest extends TestCase {
 
 	private static final String TEST_BUNDLE = "test.bundle";
+
+	private static final String LOADER_1_BUNDLE = "loader.1.bundle";
+
+	private static final String LOADER_2_BUNDLE = "loader.2.bundle";
 
 	private static final String SPANISH = "es";
 
@@ -17,9 +24,9 @@ public class BundlesTest extends TestCase {
 	}
 
 	public void testGetString() throws Exception {
-		assertEquals( "Exit", Bundles.getString( TEST_BUNDLE, "exit", null ) );
+		assertEquals( "Exit_en", Bundles.getString( TEST_BUNDLE, "exit", null ) );
 		Locale.setDefault( new Locale( SPANISH ) );
-		assertEquals( "Salir", Bundles.getString( TEST_BUNDLE, "exit", null ) );
+		assertEquals( "Exit_es", Bundles.getString( TEST_BUNDLE, "exit", null ) );
 	}
 
 	public void testGetStringShowingKey() throws Exception {
@@ -32,10 +39,20 @@ public class BundlesTest extends TestCase {
 
 	public void testGetStringNotShowingKey() throws Exception {
 		assertEquals( null, Bundles.getString( TEST_BUNDLE, "", null, false ) );
-		assertEquals( "Exit", Bundles.getString( TEST_BUNDLE, "exit", null, false ) );
+		assertEquals( "Exit_en", Bundles.getString( TEST_BUNDLE, "exit", null, false ) );
 		Locale.setDefault( new Locale( SPANISH ) );
 		assertEquals( null, Bundles.getString( TEST_BUNDLE, "", null, false ) );
-		assertEquals( "Salir", Bundles.getString( TEST_BUNDLE, "exit", null, false ) );
+		assertEquals( "Exit_es", Bundles.getString( TEST_BUNDLE, "exit", null, false ) );
+	}
+
+	public void testClassLoaderHandling() throws Exception {
+		ClassLoader loader1 = new URLClassLoader( new URL[] { new File( "source/test/resources/loader1" ).toURI().toURL() } );
+		ClassLoader loader2 = new URLClassLoader( new URL[] { new File( "source/test/resources/loader2" ).toURI().toURL() } );
+		
+		Bundles.register( loader1 );
+		Bundles.register( loader2 );
+		String text = Bundles.getString( "bundle", "string", null, false );
+		assertEquals( "Loader 1 String", text );
 	}
 
 	public void tearDown() {
