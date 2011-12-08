8091a25b4176e75f837799ee7d7142cdb030d41f
Mark Soderquist <mark@soderquist.net>
2011-12-07 19:55:13 -0700

    Force a specific font for font tests.

--- source/test/java/com/parallelsymmetry/escape/utility/ui/FontUtilTest.java
+++ source/test/java/com/parallelsymmetry/escape/utility/ui/FontUtilTest.java
@@ -1,6 +1,7 @@
 package com.parallelsymmetry.escape.utility.ui;
 
 import java.awt.Font;
+import java.io.InputStream;
 
 import junit.framework.TestCase;
 
@@ -8,16 +9,23 @@ import org.junit.Test;
 
 public class FontUtilTest extends TestCase {
 
+	private Font baseFont;
+
+	public FontUtilTest() throws Exception {
+		InputStream input = getClass().getResourceAsStream( "/fonts/ConsolaMono.ttf" );
+		baseFont = Font.createFont( Font.TRUETYPE_FONT, input );
+	}
+
 	@Test
 	public void testFindFontForWidth() {
-		Font font = FontUtil.findFontForWidth( new Font( Font.SANS_SERIF, Font.PLAIN, 1 ), "M", 14 );
-		assertEquals( 20.494053f, font.getSize2D() );
+		Font font = FontUtil.findFontForWidth( baseFont, "M", 14 );
+		assertEquals( 31.852116f, font.getSize2D() );
 	}
 
 	@Test
 	public void testFindFontForHeight() {
-		Font font = FontUtil.findFontForHeight( new Font( Font.SANS_SERIF, Font.PLAIN, 1 ), "M", 14 );
-		assertEquals( 19.55478f, font.getSize2D() );
+		Font font = FontUtil.findFontForHeight( baseFont, "M", 14 );
+		assertEquals( 19.116705f, font.getSize2D() );
 	}
 
 }
