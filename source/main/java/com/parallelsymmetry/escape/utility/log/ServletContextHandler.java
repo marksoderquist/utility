055e83f583370d7b4aeeae08454068f166533ef5
Mark Soderquist <mark@soderquist.net>
2012-01-04 19:40:33 -0700

    Add ServletContextHandler class.

---
+++ source/main/java/com/parallelsymmetry/escape/utility/log/ServletContextHandler.java
@@ -0,0 +1,37 @@
+package com.parallelsymmetry.escape.utility.log;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+import javax.servlet.ServletContext;
+
+public class ServletContextHandler extends Handler {
+
+	private ServletContext context;
+
+	public ServletContextHandler( ServletContext context ) {
+		this.context = context;
+	}
+
+	@Override
+	public void publish( LogRecord record ) {
+		if( this.context == null ) return;
+
+		if( record.getThrown() == null ) {
+			this.context.log( record.getMessage() );
+		} else {
+			this.context.log( record.getMessage(), record.getThrown() );
+		}
+	}
+
+	@Override
+	public void flush() {
+		// Intentionally do nothing.
+	}
+
+	@Override
+	public void close() throws SecurityException {
+		// Intentionally do nothing.
+	}
+
+}
