52d43e28ef4fcecc118ea34ca9d5395d6e2a7eee
Mark Soderquist <mark@soderquist.net>
2012-02-15 18:02:47 -0700

    Fix the default handler to not close console streams.

--- source/main/java/com/parallelsymmetry/escape/utility/log/DefaultHandler.java
+++ source/main/java/com/parallelsymmetry/escape/utility/log/DefaultHandler.java
@@ -6,14 +6,27 @@ import java.util.logging.StreamHandler;
 
 public class DefaultHandler extends StreamHandler {
 
+	private OutputStream stream;
+
 	public DefaultHandler( OutputStream stream ) {
 		super( stream, new DefaultFormatter() );
+		this.stream = stream;
 	}
 
 	@Override
 	public void publish( LogRecord record ) {
 		super.publish( record );
 		super.flush();
+	}
+
+	@Override
+	public synchronized void close() throws SecurityException {
+		if( stream == System.out || stream == System.err ) {
+			// If the stream is one of the console streams just flush, don't close.
+			super.flush();
+		} else {
+			super.close();
+		}
 	}
 
 }
