6bc6999577176dfa51febbe8271518042a59c094
Mark Soderquist <mark@soderquist.net>
2012-07-25 16:04:51 -0600

    More work to clean up resource locations during testing.

--- source/main/java/com/parallelsymmetry/escape/utility/CommonFlag.java
+++ source/main/java/com/parallelsymmetry/escape/utility/CommonFlag.java
@@ -3,6 +3,8 @@ package com.parallelsymmetry.escape.utility;
 public interface CommonFlag {
 
 	public static final String HELP = "-help";
+	
+	public static final String HOME = "-home";
 
 	public static final String VERSION = "-version";
 
