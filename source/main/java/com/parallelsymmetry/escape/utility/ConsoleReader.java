c70fc82cf39819a5d564495110551915837d8ba9
Mark Soderquist <mark@soderquist.net>
2012-10-08 21:48:53 -0600

    Enhance ConsoleReader with new methods.

--- source/main/java/com/parallelsymmetry/escape/utility/ConsoleReader.java
+++ source/main/java/com/parallelsymmetry/escape/utility/ConsoleReader.java
@@ -2,14 +2,13 @@ package com.parallelsymmetry.escape.utility;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
 public final class ConsoleReader extends Thread {
 
-	private InputStream input;
+	private Process process;
 
 	private OutputStream output;
 
@@ -19,23 +18,38 @@ public final class ConsoleReader extends Thread {
 
 	public ConsoleReader( Process process, OutputStream output ) {
 		super( "Console Reader" );
-		this.input = process.getInputStream();
+		this.process = process;
 		this.output = output;
 		setDaemon( true );
+	}
+
+	public boolean isProcessRunning() {
+		try {
+			process.exitValue();
+			return false;
+		} catch( IllegalThreadStateException exception ) {
+			return true;
+		}
+	}
+
+	public Process getProcess() {
+		return process;
 	}
 
 	@Override
 	public void run() {
 		String line = null;
+		String errLine = null;
+		PrintStream printer = new PrintStream( output );
+		BufferedReader reader = new BufferedReader( new InputStreamReader( process.getInputStream() ) );
+		BufferedReader errReader = new BufferedReader( new InputStreamReader( process.getErrorStream() ) );
 		try {
-			BufferedReader reader = new BufferedReader( new InputStreamReader( input ) );
-			PrintStream printer = new PrintStream( output );
-			while( ( line = reader.readLine() ) != null ) {
-				printer.println( line );
+			while( (errLine = errReader.readLine()) != null || ( line = reader.readLine() ) != null ) {
+				if( errLine != null ) printer.println( errLine );
+				if( line != null) printer.println( line );
 			}
 		} catch( IOException exception ) {
-			exception.printStackTrace();
+			exception.printStackTrace(printer);
 		}
 	}
-
 }
