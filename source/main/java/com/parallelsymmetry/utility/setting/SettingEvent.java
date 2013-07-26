cf5df6acc5bd629b7243fe8eb227444f9c8e78ec
Mark Soderquist <mark@soderquist.net>
2013-07-26 17:06:42 -0600

    Add JavaDoc to the SettingEvent class

--- source/main/java/com/parallelsymmetry/utility/setting/SettingEvent.java
+++ source/main/java/com/parallelsymmetry/utility/setting/SettingEvent.java
@@ -12,6 +12,13 @@ public class SettingEvent {
 
 	private String newValue;
 
+	/**
+	 * Create a SettingEvent.
+	 * 
+	 * @param path The full path to the setting that changed including the key.
+	 * @param oldValue The old value of the setting.
+	 * @param newValue The new value of the setting.
+	 */
 	public SettingEvent( String path, String oldValue, String newValue ) {
 		this.fullPath = path;
 		this.oldValue = oldValue;
@@ -21,22 +28,47 @@ public class SettingEvent {
 		this.key = Settings.getSettingKey( path );
 	}
 
+	/**
+	 * Get the full path of the setting that changed.
+	 * 
+	 * @return
+	 */
 	public String getFullPath() {
 		return fullPath;
 	}
 
+	/**
+	 * Get the containing node path to the setting that changed.
+	 * 
+	 * @return
+	 */
 	public String getNodePath() {
 		return nodePath;
 	}
 
+	/**
+	 * Get the key to the setting that changed.
+	 * 
+	 * @return
+	 */
 	public String getKey() {
 		return key;
 	}
 
+	/**
+	 * Get the old value of the setting.
+	 * 
+	 * @return
+	 */
 	public String getOldValue() {
 		return oldValue;
 	}
 
+	/**
+	 * Get the new value of the setting.
+	 * 
+	 * @return
+	 */
 	public String getNewValue() {
 		return newValue;
 	}
