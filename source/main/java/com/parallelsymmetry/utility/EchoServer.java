6df7a44edc1e135cd4d9ad84dd33de455cdf4b49
Mark Soderquist <mark@soderquist.net>
2014-09-05 21:57:40 -0600

    Cleanup

--- source/main/java/com/parallelsymmetry/utility/EchoServer.java
+++ source/main/java/com/parallelsymmetry/utility/EchoServer.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
