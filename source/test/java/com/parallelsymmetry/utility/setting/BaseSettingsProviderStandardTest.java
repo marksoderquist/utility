ff68af11ddc10ccbc8a257ddbdaba82c42d23a95
Mark Soderquist <mark@soderquist.net>
2015-03-21 05:05:15 -0600

    Deprecate use of DescriptorSettingsProvider

--- source/test/java/com/parallelsymmetry/utility/setting/BaseSettingsProviderStandardTest.java
+++ source/test/java/com/parallelsymmetry/utility/setting/BaseSettingsProviderStandardTest.java
@@ -1,47 +1,21 @@
 package com.parallelsymmetry.utility.setting;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-import com.parallelsymmetry.utility.Descriptor;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseSettingsProviderStandardTest extends SettingsProviderStandardTest {
 
 	@Override
 	public void setUp() throws Exception {
-		Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+		Map<String, String> map = new HashMap<String, String>();
+		map.put( "/key1", "value1" );
+		map.put( "/key2", "value2" );
+		map.put( "/key3", "value3" );
+		map.put( "/path/subkey1", "subvalue1" );
+		map.put( "/path/subkey2", "subvalue2" );
+		map.put( "/path/subkey3", "subvalue3" );
 
-		Element root = document.createElement( "settings" );
-		document.appendChild( root );
-
-		Element key1 = document.createElement( "key1" );
-		key1.setTextContent( "value1" );
-		Element key2 = document.createElement( "key2" );
-		key2.setTextContent( "value2" );
-		Element key3 = document.createElement( "key3" );
-		key3.setTextContent( "value3" );
-
-		root.appendChild( key1 );
-		root.appendChild( key2 );
-		root.appendChild( key3 );
-
-		Element path = document.createElement( "path" );
-		root.appendChild( path );
-
-		Element subkey1 = document.createElement( "subkey1" );
-		subkey1.setTextContent( "subvalue1" );
-		Element subkey2 = document.createElement( "subkey2" );
-		subkey2.setTextContent( "subvalue2" );
-		Element subkey3 = document.createElement( "subkey3" );
-		subkey3.setTextContent( "subvalue3" );
-
-		path.appendChild( subkey1 );
-		path.appendChild( subkey2 );
-		path.appendChild( subkey3 );
-
-		provider = new BaseSettingsProvider( new DescriptorSettingsProvider( new Descriptor( document ), true ) );
+		provider = new BaseSettingsProvider( new MapSettingsProvider( map ) );
 	}
 
 }
