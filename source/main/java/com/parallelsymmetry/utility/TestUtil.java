5dd29f4afe59f49d6869206805648d568cfbeb31
Mark Soderquist <mark@soderquist.net>
2018-09-05 18:52:57 -0600

    Remove dependency on utility-comm

--- source/main/java/com/parallelsymmetry/utility/TestUtil.java
+++ source/main/java/com/parallelsymmetry/utility/TestUtil.java
@@ -1,20 +1,32 @@
 package com.parallelsymmetry.utility;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class TestUtil {
 
+	private static Set<String> runners;
+
 	private static Boolean test;
 
+	static {
+		runners = new HashSet<String>();
+		runners.add( "org.junit.runners.ParentRunner");
+		runners.add( "org.junit.runner.JUnitCore" );
+		runners.add( "junit.framework.TestSuite" );
+		runners.add( "org.apache.maven.surefire.junit4.JUnit4Provider");
+	}
+
 	/**
-	 * Check if the main thread is running in the JUnit test framework. This is
-	 * done by searching the main thread stack for the class
-	 * junit.framework.TestSuite and caching the result. If the TestSuite class is
-	 * found the method will return true.
-	 * 
-	 * @return
+	 * Check if the any thread is running in the JUnit test framework. This is
+	 * done by searching the thread stack for the class junit.framework.TestSuite
+	 * and caching the result. If the TestSuite class is found the method will
+	 * return true.
+	 *
+	 * @return If any thread is being run by JUnit
 	 */
-	public static final boolean isTest() {
+	public static boolean isTest() {
 		if( test != null ) return test;
 
 		boolean result = false;
@@ -23,10 +35,10 @@ public class TestUtil {
 			if( thread.getId() == 1 ) {
 				StackTraceElement[] elements = stacks.get( thread );
 				for( StackTraceElement element : elements ) {
-					if( "junit.framework.TestSuite".equals( element.getClassName() ) ) {
+					if( runners.contains( element.getClassName() ) ) {
 						result = true;
 						break;
-					};
+					}
 				}
 			}
 		}
